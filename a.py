cc27330557327c90384f26b0ef8793e8183ee331
tianli <34931204+zengtianli@users.noreply.github.com>
2023-12-18 07:34:16 +0800

    "Added combine_files function to mike_gui.py"

---
+++ a.py
@@ -0,0 +1,11 @@
+# easy python code as example
+# run_conversion_module()
+#
+# # To execute all functions in the Processing Module, just call:
+# run_processing_module()
+#
+# # To execute all functions in the Virtual Section Management Module, just call:
+# run_virtual_section_module()
+#
+# # To execute all functions in the Conversion Module, just call: 
+# run_conversion_module()
