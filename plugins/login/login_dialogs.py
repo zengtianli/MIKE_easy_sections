db74feeb7b4d0c3db676d03cf74b3ec4eb033e3c
tianli <34931204+zengtianli@users.noreply.github.com>
2024-01-16 08:21:03 +0800

    before change password

---
+++ plugins/login/login_dialogs.py
@@ -0,0 +1,112 @@
+from PyQt6.QtWidgets import QDialog, QLineEdit, QPushButton, QVBoxLayout, QLabel, QMenuBar, QMainWindow,QMessageBox
+from PyQt6.QtGui import QAction
+from plugin_interface import PluginInterface
+from constants import PLUGIN_CONFIG_FILE
+# Add this to your login_dialogs.py file
+class SignUpDialog(QDialog):
+    def __init__(self, register_callback, parent=None):
+        super().__init__(parent)
+        self.register_callback = register_callback
+        self.setWindowTitle("Sign Up")
+        layout = QVBoxLayout(self)
+        self.username_edit = QLineEdit(self)
+        self.username_edit.setPlaceholderText("Username")
+        layout.addWidget(self.username_edit)
+        self.password_edit = QLineEdit(self)
+        self.password_edit.setPlaceholderText("Password")
+        self.password_edit.setEchoMode(QLineEdit.EchoMode.Password)
+        layout.addWidget(self.password_edit)
+        self.password_confirm_edit = QLineEdit(self)
+        self.password_confirm_edit.setPlaceholderText("Confirm Password")
+        self.password_confirm_edit.setEchoMode(QLineEdit.EchoMode.Password)
+        layout.addWidget(self.password_confirm_edit)
+        signup_button = QPushButton("Sign Up", self)
+        signup_button.clicked.connect(self.handle_signup)
+        layout.addWidget(signup_button)
+    def handle_signup(self):
+        username = self.username_edit.text()
+        password = self.password_edit.text()
+        password_confirm = self.password_confirm_edit.text()
+        if password != password_confirm:
+            QMessageBox.warning(self, "Sign Up Failed", "Passwords do not match.")
+            return
+        if self.register_callback(username, password):
+            self.accept()
+        else:
+            QMessageBox.warning(self, "Sign Up Failed", "Could not create account.")
+class LoginDialog(QDialog):
+    def __init__(self, authenticate_callback, forget_password_callback, parent=None):
+        super().__init__(parent)
+        self.authenticate_callback = authenticate_callback
+        self.forget_password_callback = forget_password_callback
+        self.setWindowTitle("Login")
+        self.setup_ui()
+    def setup_ui(self):
+        layout = QVBoxLayout(self)
+        self.username_edit = QLineEdit(self)
+        self.username_edit.setPlaceholderText("Username")
+        layout.addWidget(self.username_edit)
+        self.password_edit = QLineEdit(self)
+        self.password_edit.setPlaceholderText("Password")
+        self.password_edit.setEchoMode(QLineEdit.EchoMode.Password)
+        layout.addWidget(self.password_edit)
+        login_button = QPushButton("Login", self)
+        login_button.clicked.connect(self.handle_login)
+        layout.addWidget(login_button)
+        forget_password_button = QPushButton("Forget Password", self)
+        forget_password_button.clicked.connect(self.handle_forget_password)
+        layout.addWidget(forget_password_button)
+    def handle_login(self):
+        username = self.username_edit.text()
+        password = self.password_edit.text()
+        if self.authenticate_callback(username, password):
+            self.accept()  # Close the dialog only on successful login
+        else:
+            QMessageBox.warning(self, "Login Failed", "Incorrect username or password. Please try again.")
+    def handle_forget_password(self):
+        self.forget_password_callback()
+class ForgetPasswordDialog(QDialog):
+    def __init__(self, reset_password_callback, parent=None):
+        super().__init__(parent)
+        self.reset_password_callback = reset_password_callback
+        self.setWindowTitle("Forget Password")
+        layout = QVBoxLayout(self)
+        self.username_edit = QLineEdit(self)
+        self.username_edit.setPlaceholderText("Enter your username")
+        layout.addWidget(self.username_edit)
+        reset_button = QPushButton("Reset Password", self)
+        reset_button.clicked.connect(self.handle_reset_password)
+        layout.addWidget(reset_button)
+    def handle_reset_password(self):
+        username = self.username_edit.text()
+        if self.reset_password_callback(username):
+            self.accept()
+class ChangePasswordDialog(QDialog):
+    def __init__(self, change_password_callback, parent=None):
+        super().__init__(parent)
+        self.change_password_callback = change_password_callback
+        self.setWindowTitle("Change Password")
+        layout = QVBoxLayout(self)
+        self.old_password_edit = QLineEdit(self)
+        self.old_password_edit.setPlaceholderText("Old Password")
+        self.old_password_edit.setEchoMode(QLineEdit.EchoMode.Password)
+        layout.addWidget(self.old_password_edit)
+        self.new_password_edit = QLineEdit(self)
+        self.new_password_edit.setPlaceholderText("New Password")
+        self.new_password_edit.setEchoMode(QLineEdit.EchoMode.Password)
+        layout.addWidget(self.new_password_edit)
+        self.confirm_new_password_edit = QLineEdit(self)
+        self.confirm_new_password_edit.setPlaceholderText("Confirm New Password")
+        self.confirm_new_password_edit.setEchoMode(QLineEdit.EchoMode.Password)
+        layout.addWidget(self.confirm_new_password_edit)
+        change_password_button = QPushButton("Change Password", self)
+        change_password_button.clicked.connect(self.handle_change_password)
+        layout.addWidget(change_password_button)
+    def handle_change_password(self):
+        old_password = self.old_password_edit.text()
+        new_password = self.new_password_edit.text()
+        confirm_new_password = self.confirm_new_password_edit.text()
+        if new_password != confirm_new_password:
+            QMessageBox.warning(self, "Change Password Failed", "New passwords do not match.")
+            return
+        self.change_password_callback(old_password, new_password)
--- plugins/login_plug.py
+++ plugins/login_plug.py
@@ -1,71 +1,20 @@
-# plugins/login_plugin.py
-from PyQt6.QtWidgets import QDialog, QLineEdit, QPushButton, QVBoxLayout, QLabel, QMenuBar, QMainWindow,QMessageBox
+from PyQt6.QtWidgets import QMenuBar, QMainWindow,QMessageBox
 from PyQt6.QtGui import QAction
 from plugin_interface import PluginInterface
 from constants import PLUGIN_CONFIG_FILE
 import json
-import os
+import os,sys
 import bcrypt,base64
-class SignUpDialog(QDialog):
-    def __init__(self, register_callback, parent=None):
-        super().__init__(parent)
-        self.register_callback = register_callback
-        self.setWindowTitle("Sign Up")
-        layout = QVBoxLayout(self)
-        self.username_edit = QLineEdit(self)
-        self.username_edit.setPlaceholderText("Username")
-        layout.addWidget(self.username_edit)
-        self.password_edit = QLineEdit(self)
-        self.password_edit.setPlaceholderText("Password")
-        self.password_edit.setEchoMode(QLineEdit.EchoMode.Password)
-        layout.addWidget(self.password_edit)
-        self.password_confirm_edit = QLineEdit(self)
-        self.password_confirm_edit.setPlaceholderText("Confirm Password")
-        self.password_confirm_edit.setEchoMode(QLineEdit.EchoMode.Password)
-        layout.addWidget(self.password_confirm_edit)
-        signup_button = QPushButton("Sign Up", self)
-        signup_button.clicked.connect(self.handle_signup)
-        layout.addWidget(signup_button)
-    def handle_signup(self):
-        username = self.username_edit.text()
-        password = self.password_edit.text()
-        password_confirm = self.password_confirm_edit.text()
-        if password != password_confirm:
-            QMessageBox.warning(self, "Sign Up Failed", "Passwords do not match.")
-            return
-        if self.register_callback(username, password):
-            self.accept()
-        else:
-            QMessageBox.warning(self, "Sign Up Failed", "Could not create account.")
-class LoginDialog(QDialog):
-    def __init__(self, authenticate_callback, parent=None):
-        super().__init__(parent)
-        self.authenticate_callback = authenticate_callback
-        self.setWindowTitle("Login")
-        layout = QVBoxLayout(self)
-        self.username_edit = QLineEdit(self)
-        self.username_edit.setPlaceholderText("Username")
-        layout.addWidget(self.username_edit)
-        self.password_edit = QLineEdit(self)
-        self.password_edit.setPlaceholderText("Password")
-        self.password_edit.setEchoMode(QLineEdit.EchoMode.Password)
-        layout.addWidget(self.password_edit)
-        login_button = QPushButton("Login", self)
-        login_button.clicked.connect(self.handle_login)
-        layout.addWidget(login_button)
-    def handle_login(self):
-        username = self.username_edit.text()
-        password = self.password_edit.text()
-        if self.authenticate_callback(username, password):
-            self.accept()  # Close the dialog only on successful login
-        else:
-            QMessageBox.warning(self, "Login Failed", "Incorrect username or password. Please try again.")
+import string  # Import the string module
+import random  # Import the random module
+from plugins.login.login_dialogs import SignUpDialog, LoginDialog, ForgetPasswordDialog, ChangePasswordDialog
+BASE_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
+users_file = os.path.join(BASE_DIR,'plugins','login', 'users.json')
 class Plugin(PluginInterface):
     def __init__(self):
         self.account = None  # Holds account details
-        self.users_file = "users.json"  # File to store user data
+        self.users_file = users_file # File to store user data
         self.users = self.load_users()  # Load users from the file
-
     def register_user(self, username, password):
         if not self.is_valid_username(username):
             QMessageBox.warning(self.window, "Registration Failed", "Invalid username. It should be alphanumeric.")
@@ -76,40 +25,29 @@ class Plugin(PluginInterface):
         if username in self.users:
             QMessageBox.warning(self.window, "Registration Failed", "User already exists.")
             return False  # User already exists
-        # Hash the password before saving (to be implemented later)
         hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
         hashed_password_str = base64.b64encode(hashed_password).decode('utf-8')
         self.users[username] = hashed_password_str
-        # QMessageBox.information(self.window, "Registration Successful", "You have successfully registered.")
-
-        # Customized message with HTML formatting
         message = f"Congratulation <b>{username}</b>, you have successfully registered."
         QMessageBox.information(self.window, "Registration Successful", message)
         self.save_users()
         return True
-
     @staticmethod
     def is_valid_username(username):
         return username.isalnum()
     @staticmethod
     def is_valid_password(password):
         return len(password) >= 1
-
     def authenticate_user(self, username, password):
         user_password_hash_str = self.users.get(username)
         if user_password_hash_str:
             user_password_hash = base64.b64decode(user_password_hash_str)
             if bcrypt.checkpw(password.encode(), user_password_hash):
-                # Successful authentication
                 self.set_account({'username': username})
-                QMessageBox.information(self.window, "Login Success", "You are now logged in.")
+                QMessageBox.information(self.window, "Login Success", f"{username}, You are now logged in.")
                 self.save_config()
                 return True
-
-        # Authentication failed
-        QMessageBox.warning(self.window, "Login Failed", "Incorrect username or password. Please try again.")
         return False
-
     def initialize(self, window: QMainWindow, menu: QMenuBar):
         self.window = window
         self.create_login_menu(menu)
@@ -117,45 +55,63 @@ class Plugin(PluginInterface):
         self.signup_action = QAction('Sign Up', self.window)
         self.signup_action.triggered.connect(self.show_signup_dialog)
         self.login_menu.addAction(self.signup_action)
-    def show_signup_dialog(self):
-        dialog = SignUpDialog(self.register_user, self.window)
+    def deinitialize(self, window, menu): menu.removeAction(self.login_menu.menuAction())
+    def show_signup_dialog(self): dialog = SignUpDialog(self.register_user, self.window); dialog.exec();
+    def show_forget_password_dialog(self): dialog = ForgetPasswordDialog(self.reset_password, self.window); dialog.exec();
+    def show_login_dialog(self): dialog = LoginDialog(self.authenticate_user, self.handle_forget_password, self.window); dialog.exec();
+    def show_change_password_dialog(self):
+        dialog = ChangePasswordDialog(self.change_password, self.window)
         dialog.exec()
+    def change_password(self, old_password, new_password):
+        if not self.authenticate_user(self.account['username'], old_password):
+            QMessageBox.warning(self.window, "Change Password Failed", "Old password is incorrect.")
+            return
+        if not self.is_valid_password(new_password):
+            QMessageBox.warning(self.window, "Change Password Failed", "Invalid new password. It should be at least 8 characters long.")
+            return
+        hashed_new_password = bcrypt.hashpw(new_password.encode(), bcrypt.gensalt())
+        hashed_new_password_str = base64.b64encode(hashed_new_password).decode('utf-8')
+        self.users[self.account['username']] = hashed_new_password_str
+        self.save_users()
+        QMessageBox.information(self.window, "Change Password", "Your password has been successfully changed.")
+    def handle_forget_password(self):
+        forget_password_dialog = ForgetPasswordDialog(self.reset_password, self.window)
+        forget_password_dialog.exec()
+    def reset_password(self, username):
+        if username not in self.users:
+            QMessageBox.warning(self.window, "Reset Password Failed", "User does not exist.")
+            return False
+        temp_password = self.generate_temp_password()
+        hashed_temp_password = bcrypt.hashpw(temp_password.encode(), bcrypt.gensalt())
+        hashed_temp_password_str = base64.b64encode(hashed_temp_password).decode('utf-8')
+        self.users[username] = hashed_temp_password_str
+        self.save_users()
+        print(f"Temporary password for {username}: {temp_password}")  # For demonstration only
+        QMessageBox.information(self.window, "Reset Password", "Your password has been reset. Please check your email for the temporary password.")
+        return True
+    @staticmethod
+    def generate_temp_password(length=10):
+        characters = string.ascii_letters + string.digits
+        return ''.join(random.choice(characters) for i in range(length))
+    def update_user_menu(self, username):
+        if hasattr(self, 'user_menu'):
+            self.login_menu.removeAction(self.user_menu.menuAction())
+        self.user_menu = self.login_menu.addMenu(username)
+        logout_action = QAction('Logout', self.window)
+        logout_action.triggered.connect(self.logout)
+        self.user_menu.addAction(logout_action)
+        change_password_action = QAction('Change Password', self.window)
+        change_password_action.triggered.connect(self.show_change_password_dialog)
+        self.user_menu.addAction(change_password_action)
     def create_login_menu(self, menu):
         self.login_menu = menu.addMenu('Login')
         self.login_action = QAction('Login', self.window)
         self.login_action.triggered.connect(self.show_login_dialog)
         self.login_menu.addAction(self.login_action)
-    def show_login_dialog(self):
-        dialog = LoginDialog(self.authenticate_user, self.window)
-        dialog.exec()
-
-
-    # def authenticate_user(self, username, password):
-    #     if username == "admin" and password == "a":
-    #         self.set_account({'username': username})
-    #         print("Account set, now saving...")  # Debugging print
-    #         self.save_config()
-    #         return True
-    #     else:
-    #         return False
-
-    def deinitialize(self, window, menu):
-        menu.removeAction(self.login_menu.menuAction())
     def set_account(self, account_info):
         self.account = account_info
         self.update_user_menu(account_info['username'])  # Update the menu with the user's name
         self.save_config()
-    def update_user_menu(self, username):
-        # Remove the old user menu if it exists
-        if hasattr(self, 'user_menu'):
-            self.login_menu.removeAction(self.user_menu.menuAction())
-        # Create a new menu item with the user's name
-        self.user_menu = self.login_menu.addMenu(username)
-        # Add any actions you want to this menu
-        # For example, a logout action
-        logout_action = QAction('Logout', self.window)
-        logout_action.triggered.connect(self.logout)
-        self.user_menu.addAction(logout_action)
     def save_config(self):
         config_key = 'login_plugin'  # Unique key for this plugin's settings
         config = {}
@@ -171,7 +127,6 @@ class Plugin(PluginInterface):
             if self.account and 'username' in self.account:
                 self.update_user_menu(self.account['username'])
         else:
-            # Existing logic for loading settings directly from the config file
             if os.path.exists(PLUGIN_CONFIG_FILE):
                 with open(PLUGIN_CONFIG_FILE, 'r') as file:
                     config = json.load(file)
@@ -180,7 +135,6 @@ class Plugin(PluginInterface):
                     if self.account and 'username' in self.account:
                         self.update_user_menu(self.account['username'])
     def logout(self):
-        # Reset the account information and update the menu
         self.account = None
         self.update_user_menu('')
     def load_users(self):
